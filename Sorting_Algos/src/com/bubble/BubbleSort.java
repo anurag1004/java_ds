9168601c6bf27ce7341e834ec78212d35d372ad4
anurag <anuragv.1020@gmail.com>
2020-10-15 09:46:24 +0530

    init commit

---
+++ Sorting_Algos/src/com/bubble/BubbleSort.java
@@ -0,0 +1,32 @@
+package com.bubble;
+
+import java.util.ArrayList;
+
+public class BubbleSort{
+    // Worst Case O(n^2) [if array is in descending order and we want ascending order or vice versa]
+    // Best case (if already sorted) O(n)
+    // Average case O(n^2) [If array elements are all jumbled i.e neither ascending nor descending]
+    public static void sort(int[] arr){
+        boolean swapped;
+        for(int i=0;i<arr.length-1;i++){
+            swapped = false;
+            for(int j=0;j< arr.length -1 -i;j++){
+                if(arr[j]>arr[j+1]){
+                    //swap
+                    int temp =  arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(swapped==false) break;
+        }
+    }
+    public static void main(String[] args){
+      int[] arr = {1,2,3,4,5};
+      sort(arr);
+      for(int e:arr){
+          System.out.print(" "+e);
+      }
+    }
+}
---
+++ Sorting_Algos/src/com/selection/SelectionSort.java
@@ -0,0 +1,31 @@
+package com.selection;
+
+public class SelectionSort {
+    public void selectionSort(int[] arr){
+        int pos = 0;
+        int n = arr.length;
+        int min; // to store minimum element's index
+        while(pos<n-1){
+            // pos is first index of unsortedList
+            min = pos;
+            for(int i=pos+1;i<n;i++){
+                if(arr[i]<arr[min])
+                    min = i;
+            }
+            // swap min with pos
+            int temp = arr[pos];
+            arr[pos] = arr[min];
+            arr[min] = temp;
+
+            pos++;
+        }
+    }
+    public static void main(String[] args){
+        int[] arr = {4,5,6,0,2,10,90,-90,234,5,0,45};
+        SelectionSort ss = new SelectionSort();
+        ss.selectionSort(arr);
+        for(int x:arr){
+            System.out.print(" "+x);
+        }
+    }
+}
