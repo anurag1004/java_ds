cc53bbd35d7a851367b35476884454846bb1b1af
anurag <anuragv.1020@gmail.com>
2020-10-26 13:12:53 +0530

    Finding Largest Common Prefix in an array of Strings using Divide and Conquer [merge Sort approach]

---
+++ LCS/src/com/divideAndConquer/Main.java
@@ -0,0 +1,35 @@
+package com.divideAndConquer;
+
+public class Main {
+    static private String commonPrefix(String str1,String str2){
+
+        StringBuilder prefix = new StringBuilder();
+        int l1 = str1.length();
+        int l2 = str2.length();
+        int i,j;
+        i=0;
+        j=0;
+        while((i < l1 && j < l2) && str1.charAt(i) == str2.charAt(j)){
+            prefix.append(str1.charAt(i));
+            i++;
+            j++;
+        }
+        return prefix.toString();
+    }
+    static public String LCP(String[] strs,int p,int r){
+        if(p==r) return strs[p];
+        while(p<r){
+            int q = (p+r)/2;
+            String str1 = LCP(strs,p,q);
+            String str2 = LCP(strs,q+1,r);
+
+            return commonPrefix(str1,str2);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"l",""};
+        System.out.println(LCP(strs,0,strs.length-1));
+    }
+}
