8372b0ed0a8e9a65785cb86febbc27e93d5667f4
anurag <anuragv.1020@gmail.com>
2020-11-04 03:42:51 +0530

    all path source to target using backtracking

---
+++ GraphBasedAlgo/src/com/allPathSrcToTarget/Main.java
@@ -0,0 +1,43 @@
+package com.allPathSrcToTarget;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static void dfsUtil(int v, int dest, int[][] graph, boolean[] visited, List<Integer> path, List<List<Integer>> out){
+        if(v==dest){
+            out.add(new ArrayList<>(path)); // copying the path
+            return;
+        }
+        visited[v] = true;
+        for(int i=0;i<graph[v].length;i++){
+            if(!visited[i] && graph[v][i]==1){
+                path.add(i);
+                dfsUtil(i, dest, graph, visited, path, out);
+                path.remove(path.size()-1); // backtrack
+            }
+        }
+        visited[v]  =  false; // backtrack
+        return;
+    }
+    public static void allPathsFromSrcToTarget(int[][] graph,int src){
+        boolean[] visited = new boolean[graph.length];
+        int goal = graph.length-1;
+        List<List<Integer>> out = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        path.add(src); // adding starting point
+        dfsUtil(src,goal,graph,visited,path,out);
+
+        System.out.println(out.toString());
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {{0,1,1,0,0,0},
+                        {1,0,0,1,0,0},
+                        {1,0,0,1,1,0},
+                        {0,1,1,0,1,0},
+                        {0,0,1,1,0,1},
+                        {0,0,0,0,1,0}};
+        allPathsFromSrcToTarget(graph,0);
+    }
+}
