c7ffb1faf593a4e5cdfe9eef3a44952f5a036ee5
anurag <anuragv.1020@gmail.com>
2021-04-14 18:55:25 +0530

    added cycle detection algo for directed graph

--- Graph_2/src/com/main/Main.java
+++ Graph_2/src/com/main/Main.java
@@ -23,6 +23,13 @@ public class Main {
             }
         }
     }
+    public static void dfsRecur(int[][] graph, int v, boolean[] visited){
+        visited[v] = true;
+        System.out.print(v+"->");
+        for(int n: graph[v]){
+            if(!visited[n]) dfsRecur(graph, n, visited);
+        }
+    }
     public static void dfs(int[][] graph, int src){
         Stack<Integer> stack = new Stack<>();
         boolean[] visited = new boolean[graph.length];
@@ -41,13 +48,37 @@ public class Main {
             }
         }
     }
+    private static boolean isCyclic_recu(int[][] graph, boolean[] visited, boolean[] recStack, int v){
+        if(recStack[v]){
+            // if already in the recursion stack, means we reached the previously visited node again
+            // cycle exists
+            return true;
+        }
+        if(visited[v]) // if already visited, no need to visit it again
+            return false;
+
+        visited[v] = true;
+        recStack[v] = true;
+        for(int n: graph[v]){
+                if(isCyclic_recu(graph, visited,recStack, n)) return true;
+        }
+        recStack[v] = false;
+        return false;
+    }
+    public static boolean isCyclic(int[][] graph){
+        boolean[] recStack = new boolean[graph.length];
+        boolean[] visited = new boolean[graph.length];
+        for(int i=0;i<graph.length;i++){
+            if(isCyclic_recu(graph, visited, recStack,i)) return true;
+        }
+        return false;
+    }
     public static void main(String[] args){
     // undirected
-        int[][] graph = {{1,2},{0,2},{0,1,3},{2,4,5},{3,5,6},{3,4,6},{5,4}};
+//        int[][] graph = {{1,2},{0,2},{0,1,3},{2,4,5},{3,5,6},{3,4,6},{5,4}};
     // directed graph
-    //   int[][] graph = { {1,2}, {2}, {3}, {4,5}, {5}, {} };
-        bfs(graph, 0);
-        System.out.println();
-        dfs(graph, 0);
+       int[][] graph = { {1,2}, {2}, {3}, {4,5}, {5}, {1} };
+//       dfs(graph,0);
+        System.out.println(isCyclic(graph));
     }
 }
