356379dbabcd5c57f437da17fa41645466987d34
anurag <anuragv.1020@gmail.com>
2021-04-24 12:16:40 +0530

    minimum sliding window problem

--- Strings/src/com/strings/Main.java
+++ Strings/src/com/strings/Main.java
@@ -16,10 +16,46 @@ public class Main {
             }
         }
     }
+    public static String minWindow(String s, String t){
+        if(s.length()==0 || t.length()==0) return "";
+        Map<Character, Integer>  tfreq = new HashMap<>();
+        for(char x:t.toCharArray()) tfreq.put(x, tfreq.getOrDefault(x,0)+1);
+        int left = 0;
+        int right = 0;
+        int counter = 0;
+        int target = tfreq.size();
+        Map<Character, Integer> windowCount = new HashMap<>();
+        int[] ans = {-1, 0, 0};
+        while (right<s.length()){
+            char c = s.charAt(right);
+            windowCount.put(c, windowCount.getOrDefault(c,0)+1);
+            if(tfreq.containsKey(c) && tfreq.get(c).intValue()==windowCount.get(c).intValue()){
+                counter++;
+            }
+            while(left<=right && counter==target){
+                c = s.charAt(left);
+                if(ans[0]==-1 || (right-left+1)<ans[0]){
+                    ans[0] = right-left+1;
+                    ans[1] = left;
+                    ans[2] = right;
+                }
+                windowCount.put(c, windowCount.get(c)-1);
+                if(tfreq.containsKey(c) && tfreq.get(c).intValue() >  windowCount.get(c).intValue()){
+                    counter--;
+                }
+                left++;
+            }
+            right++;
+        }
+        return ans[0]==-1?"":s.substring(ans[1], ans[2]+1);
+    }
     public static void main(String[] args) {
         String str = "abc";
         List<String> out = new ArrayList<>();
         generatePermutations(out, new StringBuilder(), str, new boolean[str.length()]);
         System.out.println(out.toString());
+        String s ="ADOBECODEBANC";
+        String t = "ABC";
+        System.out.println(minWindow(s, t));
     }
 }
