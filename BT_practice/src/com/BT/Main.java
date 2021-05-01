566d12534622beca4f107ec60c9d9f3f4fa60408
anurag <anuragv.1020@gmail.com>
2021-05-01 23:56:25 +0530

    vertical order traversal

--- BT_practice/src/com/BT/Main.java
+++ BT_practice/src/com/BT/Main.java
@@ -8,11 +8,21 @@ class TreeNode{
         this.val = val;
     }
 }
+// queue obj for vertical order traversing
+class Node{
+    TreeNode root;
+    int level; //vertical level i.e ...-2, -1, 0, 1, 2...
+    public Node(TreeNode root, int level){
+        this.root = root;
+        this.level = level;
+    }
+}
 class BST{
     TreeNode root;
     public BST(){
         root = null;
     }
+    public TreeNode getRoot(){ return this.root; }
     public void insert(int val){
         root = insertRec(root, val);
     }
@@ -167,41 +177,127 @@ class BST{
         return left;
     }
 }
-public class Main {
-    public static void main(String[] args){
-        int[] arr = {5,4,8,11,13,7,2,1,15,25,23,26,14,40,39,50};
-        BST bst = new BST();
-        for(int x:arr){
-            bst.insert(x);
+class BinaryTree{
+    TreeNode root;
+    public BinaryTree(){
+        root = null;
+    }
+    public TreeNode getRoot(){ return this.root;}
+    public void insert(int val){
+        TreeNode newNode = new TreeNode(val);
+        if(this.root==null){
+            this.root = newNode;
+            return;
         }
-        ////////////////////////////////////
-        System.out.println("Inorder");
-        bst.inOrder();
-        System.out.println();
-        bst.InorderDfsItr();
-        ////////////////////////////////////
-        System.out.println("\nPreOrder");
-        bst.preOrder();
-        System.out.println();
-        bst.PreOrderDfsItr();
-        ///////////////////////////////////
-        System.out.println();
-        System.out.print("Depth "+bst.depth()+"\n");
-        //////////////////////////////////
-        bst.bfs();
-        //////////////////////////////////
-        System.out.println();
-        System.out.println(bst.findDistanceFromParent(bst.root, 14, 0));
-        //////////////////////////////////
-        TreeNode lcs = bst.LCS(14,39,bst.root);
-        System.out.println(bst.findDistanceFromParent(lcs,14,0)+bst.findDistanceFromParent(lcs,39,0));
-        /////////////////////////////////
-        List<Integer> path = bst.longestPathFromRoot(bst.root);
-        Collections.reverse(path);
-        System.out.println(path.toString());
-
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(this.root);
+        // insert via level order traversal
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            while (size-->0){
+                TreeNode node = queue.poll();
+                if(node.left==null){
+                    node.left = newNode;
+                    return;
+                }
+                if(node.right==null){
+                    node.right = newNode;
+                    return;
+                }
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+    }
+    public void bfs(){
+        TreeNode curr = root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(curr);
+        while (!queue.isEmpty()){
+            curr = queue.poll();
+            System.out.print(curr.val+" ");
+            if(curr.left!=null) queue.add(curr.left);
+            if(curr.right!=null) queue.add(curr.right);
+        }
+    }
+}
+public class Main {
+    // vertical order traversing for getting top view
+    public static List<Integer> getTopView(TreeNode root){
+        Map<Integer, Integer> treeMap = new HashMap<>();
+        List<Integer> out = new LinkedList<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(root, 0));
+        treeMap.put(0, root.val);
+        out.add(root.val);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            while (size-->0){
+                Node node = queue.poll();
+                int level = node.level;
+                if(!treeMap.containsKey(level)){
+                    if(level<0)
+                        out.add(0, node.root.val);
+                    else
+                        out.add(node.root.val);
+                    treeMap.put(level, node.root.val);
+                }
+                if(node.root.left!=null)
+                    queue.add(new Node(node.root.left, level-1));
+                if(node.root.right!=null)
+                    queue.add(new Node(node.root.right, level+1));
+            }
+        }
+        return out;
+    }
+    public static List<List<Integer>> getVerticalOrderTraversal(TreeNode root){
+        if(root==null) return new ArrayList<>();
+        Map<Integer, List<Integer>> treeMap = new HashMap<>();
+        int min_key = 0;
+        int max_key = 0;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(root, 0));
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            while (size-->0){
+                Node node = queue.poll();
+                int level = node.level;
+                min_key = Math.min(level, min_key);
+                max_key = Math.max(level, max_key);
+                if(!treeMap.containsKey(level)){
+                    treeMap.put(level, new LinkedList<>(Arrays.asList(node.root.val)));
+                }else{
+                    List<Integer> temp = treeMap.get(level);
+                    temp.add(node.root.val);
+                    treeMap.put(level, temp);
+                }
+                if(node.root.left!=null)
+                    queue.add(new Node(node.root.left, level-1));
+                if(node.root.right!=null)
+                    queue.add(new Node(node.root.right, level+1));
+            }
+        }
         List<List<Integer>> out = new ArrayList<>();
-        bst.pathToAllLeafFromRoot(lcs,out,new ArrayList<Integer>());
-        System.out.println(out.toString());
+        while (min_key<=max_key){
+            out.add(treeMap.get(min_key++));
+        }
+        return out;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7};
+        BinaryTree bt = new BinaryTree();
+        for(int x:arr) bt.insert(x);
+        System.out.println("BFS");
+        bt.bfs();
+        System.out.println();
+        List<Integer> out = getTopView(bt.getRoot());
+        Iterator<Integer> it = out.iterator();
+        System.out.println("Top View");
+        while(it.hasNext()){
+            System.out.print(it.next()+" ");
+        }
+        System.out.println("\nVertical Traversal");
+        List<List<Integer>> list = getVerticalOrderTraversal(bt.getRoot());
+        System.out.println(list);
     }
 }
