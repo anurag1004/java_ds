433094fdd76acff780efb7afc9bde805d21a2f5b
anurag <anuragv.1020@gmail.com>
2021-05-09 20:13:39 +0530

    knapsack dynamic programming

--- DP/src/com/knapsack/Knapsack.java
+++ DP/src/com/knapsack/Knapsack.java
@@ -20,14 +20,40 @@ public class Knapsack {
             }
         }
     }
+    // will return max value within maximum defined weight
+    static int dynamicProgramming(int[] wt,  int[] val, int maxWt){
+        int n = wt.length;
+        int[][] dp = new int[n+1][maxWt+1];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=maxWt;j++){
+                // ask  two questions
+                // if to take this value
+                // not to take this value
+                // take max of it
+                int currMaxWt = j;
+                if(currMaxWt >= wt[i-1]){
+                    int taken = val[i-1] + dp[i-1][currMaxWt-wt[i-1]];
+                    int notTaken = dp[i-1][currMaxWt];
+                    dp[i][j] = Math.max(taken, notTaken);
+                }else{
+                    // weight can't be taken
+                    // take from above
+                    dp[i][j] = dp[i-1][currMaxWt];
+                }
+            }
+        }
+        return dp[n][maxWt];
+    }
     public static void main(String[] args) {
-        int[] wt = {10,20,30};
-        int[] val = {60,100,120};
+//        int[] wt = {12, 2, 1, 1, 4};
+//        int[] val = {4, 2, 1, 2, 10};
+//         out :- 15
+        int[] wt = {2, 5, 1, 3, 4};
+        int[] val = {15, 14, 10, 45, 30};
+//         out :- 11
         boolean[] seen = new boolean[wt.length];
-//        helper(seen, wt, val, 0, 0, 1500);
-//        System.out.println(maxVal);
-
-        // for top to down
-        Map<Integer, Integer> map = new HashMap<>();
+        helper(seen, wt, val, 0, 0, 7);
+        System.out.println(maxVal);
+        System.out.println(dynamicProgramming(wt, val, 7));
     }
 }
